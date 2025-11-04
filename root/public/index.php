de4ef8f316c8b6098dac4e4eaff969703e27b797
Shameer Sheikh <shamwork14@gmail.com>
2025-11-03 19:12:05 -0500

    wrote sample code

--- root/public/index.php
+++ root/public/index.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Hello World!</h1>";
+        echo "<p>This is a simple PHP example.</p>";
+    ?>
+</body>
+</html>
