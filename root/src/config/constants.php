4e203162b6945afaca0146d86d27bb5900d3dacf
Anika Khan <anika.khan323@gmail.com>
2025-11-10 20:55:17 -0500

    feat: added constants for easy URL handling; added header.php reusable component

---
+++ root/src/config/constants.php
@@ -0,0 +1,44 @@
+<?php
+// Base URL for project
+define('BASE_URL', '/COMP-4150-Group-Project/root/');
+
+// Path to public html/php files
+define('PUBLIC_URL', BASE_URL . 'public/');
+
+// Path to user html/php files
+define('USER_URL', BASE_URL . 'public/user/');
+
+// Path to admin html/php files
+define('ADMIN_URL', BASE_URL . 'public/admin/');
+
+// Path to club html/php files
+define('CLUB_URL', BASE_URL . 'public/club/');
+
+// Path to event html/php files
+define('EVENT_URL', BASE_URL . 'public/event/');
+
+// Path to main style script
+define('CSS_URL', BASE_URL . 'assets/styles/style.css');
+
+// Path to layout folder
+define('LAYOUT_URL', BASE_URL . 'assets/styles/layout/');
+
+// Path to template style scripts
+define('TEMPLATE_URL', BASE_URL . 'assets/styles/templates/');
+
+// Path to images folder
+define('IMG_URL', BASE_URL . 'assets/img/');
+
+// Path to config folder
+define('CONFIG_URL', BASE_URL . 'src/config/');
+
+// Path to JS scripts folder
+define('JS_URL', BASE_URL . 'src/scripts/js/');
+
+// Path to PHP scripts folder
+define('PHP_URL', BASE_URL . 'src/scripts/php/');
+
+// Path to SQL scripts folder
+define('SQL_URL', BASE_URL . 'src/scripts/sql/');
+
+?>
