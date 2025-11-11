0ba9de9180a144eac323001beb741e29b389872b
Anika Khan <anika.khan323@gmail.com>
2025-11-10 20:54:34 -0500

    chore: fixed DB name typo in config

--- root/src/config/db_config.php
+++ root/src/config/db_config.php
@@ -2,5 +2,5 @@
 define('DB_HOST', 'localhost');                 // Host name
 define('DB_USER', 'root');                      // MySQL username
 define('DB_PASS', '');                          // MySQL password
-define('DB_NAME', 'comp-4150-final-project');   // MySQL database name
+define('DB_NAME', 'comp-4150-group-project');   // MySQL database name
 ?>
