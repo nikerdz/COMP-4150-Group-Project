e061fe8042488acf1263f720082e82c2995d7fdf
Anika Khan <anika.khan323@gmail.com>
2025-11-12 00:35:58 -0500

    fix: refactored constants.php to amend bugs due to incorrect directory paths

--- root/src/config/constants.php
+++ root/src/config/constants.php
@@ -1,44 +1,39 @@
 <?php
+// Root directory
+define('ROOT_PATH', realpath(__DIR__ . '/../../') . '/');
+define('PUBLIC_PATH', ROOT_PATH . 'public/');
+define('SOURCE_PATH', ROOT_PATH . 'src/');
+define('ASSETS_PATH', ROOT_PATH . 'assets/');
+define('LAYOUT_PATH', ASSETS_PATH . 'layout/');
+
 // Base URL for project
 define('BASE_URL', '/COMP-4150-Group-Project/root/');
 
-// Path to public html/php files
-define('PUBLIC_URL', BASE_URL . 'public/');
+// ASSETS
+define('ASSETS_URL', BASE_URL . 'assets/');         // URL to assets folder
+define('IMG_URL', ASSETS_URL . 'img/');             // URL to image files
+define('LAYOUT_URL', ASSETS_URL . 'layout/');       // URL to layout files
 
-// Path to user html/php files
-define('USER_URL', BASE_URL . 'public/user/');
+// ASSETS - Styles
+define('CSS_URL', ASSETS_URL . 'styles/');          // URL to styles folder
+define('STYLE_URL', CSS_URL . 'style.css');         // URL to main style script
+define('TEMPLATES_URL', CSS_URL . 'templates/');    // URL to template style scripts
 
-// Path to admin html/php files
-define('ADMIN_URL', BASE_URL . 'public/admin/');
+// PUBLIC
+define('PUBLIC_URL', BASE_URL . 'public/');         // URL to public folder
+define('USER_URL', PUBLIC_URL . 'user/');           // URL to user html/php files
+define('ADMIN_URL', PUBLIC_URL . 'admin/');         // URL to admin html/php files
+define('CLUB_URL', PUBLIC_URL . 'club/');           // URL to club html/php files
+define('EVENT_URL', PUBLIC_URL . 'event/');         // URL to event html/php files
 
-// Path to club html/php files
-define('CLUB_URL', BASE_URL . 'public/club/');
+// SOURCE
+define('SOURCE_URL', BASE_URL . 'src/');            // URL to source folder
+define('CONFIG_URL', SOURCE_URL . 'config/');       // URL to config folder
 
-// Path to event html/php files
-define('EVENT_URL', BASE_URL . 'public/event/');
-
-// Path to main style script
-define('CSS_URL', BASE_URL . 'assets/styles/style.css');
-
-// Path to layout folder
-define('LAYOUT_URL', BASE_URL . 'assets/styles/layout/');
-
-// Path to template style scripts
-define('TEMPLATE_URL', BASE_URL . 'assets/styles/templates/');
-
-// Path to images folder
-define('IMG_URL', BASE_URL . 'assets/img/');
-
-// Path to config folder
-define('CONFIG_URL', BASE_URL . 'src/config/');
-
-// Path to JS scripts folder
-define('JS_URL', BASE_URL . 'src/scripts/js/');
-
-// Path to PHP scripts folder
-define('PHP_URL', BASE_URL . 'src/scripts/php/');
-
-// Path to SQL scripts folder
-define('SQL_URL', BASE_URL . 'src/scripts/sql/');
+// SOURCE - Scripts
+define('SCRIPTS_URL', SOURCE_URL . '/scripts');     // URL to scripts folder
+define('JS_URL', SCRIPTS_URL . 'js/');              // URL to JS scripts folder
+define('PHP_URL', SCRIPTS_URL . 'php/');            // URL to PHP scripts folder
+define('SQL_URL', SCRIPTS_URL . 'sql/');            // URL to SQL scripts folder
 
 ?>
