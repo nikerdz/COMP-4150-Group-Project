23f876436090863cf0d68f8fc43ca6695ebd3ab5
Anika Khan <anika.khan323@gmail.com>
2025-11-18 16:58:06 -0500

    feat: added templates

--- root/public/user/view-user.php
+++ root/public/user/view-user.php
@@ -1,1 +1,40 @@
 <!-- For viewing user profiles besides the logged in user -->
+ <?php
+require_once('../src/config/constants.php');
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta property="og:title" content="ClubHub - Discover Your Campus Community">
+    <meta property="og:description" content="Join ClubHub and explore clubs, events, and connect with fellow students on campus.">
+    <meta property="og:image" content="<?php echo IMG_URL; ?>logo_hub.png">
+    <meta property="og:url" content="https://khan661.myweb.cs.uwindsor.ca/COMP-4150-Group-Project/root/public/">
+    <meta property="og:type" content="website"> <!-- Enhance link previews when shared on Facebook, LinkedIn, and other platforms -->
+
+    <title>ClubHub | </title>
+
+    <link rel="icon" type="image/png" href="<?php echo IMG_URL; ?>favicon-32x32.png">
+    <link rel="stylesheet" href="<?php echo STYLE_URL; ?>?v=<?php echo time(); ?>">
+</head>
+
+<body>
+
+<?php include_once(LAYOUT_PATH . 'header.php'); ?>
+
+<main>
+
+</main>
+
+<?php include_once(LAYOUT_PATH . 'navbar.php'); ?>
+<?php include_once(LAYOUT_PATH . 'footer.php'); ?>
+
+<script src="<?php echo JS_URL; ?>script.js?v=<?php echo time(); ?>"></script>
+
+</body>
+</html>
