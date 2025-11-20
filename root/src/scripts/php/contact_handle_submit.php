f991e320911b8c73e113a5beca1fc5a935bd0e1a
Anika Khan <anika.khan323@gmail.com>
2025-11-19 23:08:26 -0500

    fix: renamed contact handle script for consistency

---
+++ root/src/scripts/php/contact_handle_submit.php
@@ -0,0 +1,32 @@
+<?php
+require_once('../../../config/constants.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $name    = htmlspecialchars($_POST['name'] ?? '');
+    $email   = htmlspecialchars($_POST['email'] ?? '');
+    $subject = htmlspecialchars($_POST['subject'] ?? '');
+    $message = htmlspecialchars($_POST['message'] ?? '');
+
+    $to = "khan661@uwindsor.ca, sheikh83@uwindsor.ca";
+
+    $headers  = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $fullMessage =
+        "New message from ClubHub Contact Form\n\n" .
+        "Name: $name\n" .
+        "Email: $email\n" .
+        "Subject: $subject\n\n" .
+        "Message:\n$message\n";
+
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        header("Location: /COMP-4150-Group-Project/root/public/contact.php?success=1");
+        exit;
+    } else {
+        header("Location: /COMP-4150-Group-Project/root/public/contact.php?error=1");
+        exit;
+    }
+}
+?>
